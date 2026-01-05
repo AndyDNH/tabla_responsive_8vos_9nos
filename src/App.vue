fb69cee465245d85e250490e6f969c7f27b47077
AndyDnh <andrescuevanw@gmail.com>
2026-01-04 23:10:45 -0500

    Revert "Unión de Archivos y Verificación de Funcionamiento" normal
    This reverts commit 9e132804e97215b7de0d57ff7a773f589d735dd2.

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <TablaGenerica
-    :datos="[
-      {
-        provincia: 'Pichincha',
-        candidato: 'LUISA GONZALEZ',
-        partido_politico: 'RC5',
-        porcentaje: '45.5%',
-      },
-      {
-        provincia: 'Guayas',
-        candidato: 'DANIELA CHACON',
-        partido_politico: 'ID',
-        porcentaje: '39.2%',
-      },
-      {
-        provincia: 'Azuay',
-        candidato: 'JORGE ARTEAGA',
-        partido_politico: 'UNES',
-        porcentaje: '50.1%',
-      },
-    ]"
-    titulo="Resultados Electorales 2024"
-    textoBotonDescarga="Descargar"
-    urlArchivoDescarga="/resultados.json"
-  />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import TablaGenerica from "./components/TablaGenerica.vue";
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    // HelloWorld
-    TablaGenerica,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
 <style>
